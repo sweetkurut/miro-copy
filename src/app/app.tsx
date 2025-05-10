aba1388b2483963e30cb194db8f88a1d103834d4
sweetkurut <devxwc@gmail.com>
2025-05-10 11:30:05 +0600

    add router and linter

---
+++ src/app/app.tsx
@@ -0,0 +1,17 @@
+import { Outlet, useLocation } from "react-router-dom";
+import { AppHeader } from "@/features/header";
+import { ROUTES } from "@/shared/model/routes";
+
+export function App() {
+  const location = useLocation();
+
+  const isAuthPage =
+    location.pathname === ROUTES.LOGIN || location.pathname === ROUTES.REGISTER;
+
+  return (
+    <div>
+      {!isAuthPage && <AppHeader />}
+      <Outlet />
+    </div>
+  );
+}
